e3e62608b0655bc1210b1fd3a1b1ca09da171319
Mohammed Jibran Akhter <jibranakhtar6@gmail.com>
2024-10-16 15:05:02 -0700

    Day 1
    Initial setup of ChavviCalc.

---
+++ chavvicalc/src/main/java/com/project/App.java
@@ -0,0 +1,7 @@
+package com.chavvicalc;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = 0, B = 0;
+        String input;
